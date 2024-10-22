41441b1dcfe41f8fd4147ab9de65ca1fbdbff4d9
Yishai Zehavi <33053646+zyishai@users.noreply.github.com>
2024-10-22 13:01:13 +0300

    test: User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  # test "should get home" do
+  #   get root_url
+  #   assert_response :success
+  # end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,57 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user email must be a valid Email address" do
+    one = users(:one)
+    one.email = "foobar.com"
+    one.valid?
+
+    assert_includes one.errors.messages[:email], "is invalid"
+
+    one.email = "one@example.com"
+    assert_equal one.valid?, true
+  end
+
+  test "email is saved in small case letters" do
+    two = users(:two)
+    two.email = "TWO@EXAMPLE.COM"
+    two.save
+
+    assert_equal two.email, "two@example.com"
+  end
+
+  test "unconfirmed email can be empty" do
+    one = users(:one)
+
+    assert_nil one.unconfirmed_email
+    assert_equal one.valid?, true
+  end
+
+  test "unconfirmed email must be a valid Email address" do
+    two = users(:two)
+    two.unconfirmed_email = "baz.org"
+    two.valid?
+
+    assert_includes two.errors.messages[:unconfirmed_email], "is invalid"
+  end
+
+  test "unconfirmed email is saved in small case letters" do
+    two = users(:two)
+    two.unconfirmed_email = "NEW@EXAMPLE.COM"
+    two.save
+
+    assert_equal two.unconfirmed_email, "new@example.com"
+  end
+
+  test "user can have zero active sessions" do
+    two = users(:two)
+
+    assert_empty two.active_sessions
+  end
+
+  test "user can have multiple active sessions" do
+    one = users(:one)
+
+    assert_equal one.active_sessions.length, 2
+  end
 end
