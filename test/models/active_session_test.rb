a352a2b7065c68b2a6f320d398c2e207171ac348
Yishai Zehavi <33053646+zyishai@users.noreply.github.com>
2024-10-22 13:02:17 +0300

    test: ActiveSession model

--- test/models/active_session_test.rb
+++ test/models/active_session_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ActiveSessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "active session must belong to a user" do
+    session = ActiveSession.new
+    session.valid?
+
+    assert_includes session.errors.messages[:user], "must exist"
+  end
+
+  test "active session have a remember token" do
+    session = ActiveSession.new(user: users(:one))
+
+    assert_not_nil session.remember_token
+    assert_not_empty session.remember_token
+  end
 end
